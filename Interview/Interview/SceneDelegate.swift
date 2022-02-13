1ff76727ce3f02ffee946306d3a832d5f4eba275
Piotr Bogus <piotr.bogus@new-gravity.com>
2022-02-13 20:21:50 +0100

    coordinator

--- Interview/Interview/SceneDelegate.swift
+++ Interview/Interview/SceneDelegate.swift
@@ -1,19 +1,18 @@
-
 import UIKit
 import Utils
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
     var window: UIWindow?
+    var appCoordinator: AppCoordinator?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = NavigationController(rootViewController: IntroBuilder().build())
         self.window = window
-        window.makeKeyAndVisible()
+        appCoordinator = AppCoordinator(window: window)
+        appCoordinator?.start()
     }
 }
 
